f43f8ac4d85e4922ffdd2242d6f2816dbf593ee7
web_roaster <84627640+webroaster@users.noreply.github.com>
2022-07-16 18:09:48 +0900

    実績リンク切れ削除

--- src/components/atoms/layout/Works.jsx
+++ src/components/atoms/layout/Works.jsx
@@ -49,7 +49,7 @@ export const Works = () => {
     const work3 = <img src="/static/fc144ffaa19331f28da52f75d9d897e9/works-img1.jpg" alt="制作一覧" />
     const work4 = <img src="/static/8bee6fa4278922d839a3d5f47c67be72/works-img.jpg" alt="制作一覧" />
     const work5 = <img src="/static/de650a2304cd4317345601dc4993230f/works-img4.jpg" alt="制作一覧" />
-    const work6 = <img src="/static/ec394bfd1304190d64a9bd7560e9a095/works-img5.jpg" alt="制作一覧" />
+    // const work6 = <img src="/static/ec394bfd1304190d64a9bd7560e9a095/works-img5.jpg" alt="制作一覧" />
     const work7 = <img src="/static/b325710bdef3ae240fd85ae60961d56f/works-img6.jpg" alt="制作一覧" />
     const work8 = <img src="/static/6e154ebd7011da0ad1f55793910bdc1f/works-img7.jpg" alt="制作一覧" />
     const text1 = <p>デザイン・WordPress構築・内部SEO</p>;
@@ -57,7 +57,7 @@ export const Works = () => {
     const text3 = <p>企画・デザイン・WordPress構築・内部SEO</p>;
     const text4 = <p>デザイン・WordPress構築・内部SEO・ブログ運営・ネットショップ連携</p>;
     const text5 = <p>デザイン・WordPress構築・内部SEO</p>;
-    const text6 = <p>企画・デザイン・WordPress構築・内部SEO</p>;
+    // const text6 = <p>企画・デザイン・WordPress構築・内部SEO</p>;
     const text7 = <p>デザイン・WordPressオリジナルテーマ・内部SEO</p>;
     const text8 = <p>企画・デザイン・WordPressオリジナルテーマ・内部SEO・予約機能</p>;
     return(
@@ -71,7 +71,7 @@ export const Works = () => {
                     <WorksCont url={"https://sgcreate1.com/"} worksImg={work3} title={"メディアサイト制作"} text={text3} />
                     <WorksCont url={"https://canoa-coffee.xyz/"} worksImg={work4} title={"自社HP・ブログ"} text={text4} />
                     <WorksCont url={"https://english-philippines.com/"} worksImg={work5} title={"オンライン英会話HP"} text={text5} />
-                    <WorksCont url={"https://livecafeotodama.net/"} worksImg={work6} title={"ミュージックバーHP"} text={text6} />
+                    {/* <WorksCont url={"https://livecafeotodama.net/"} worksImg={work6} title={"ミュージックバーHP"} text={text6} /> */}
                     <WorksCont url={"https://dress-maker.com/"} worksImg={work7} title={"人材募集LP"} text={text7} />
                     <WorksCont url={"https://hibiki-music-studio.com/"} worksImg={work8} title={"ミュージックスタジオHP"} text={text8} />
                     <div></div>
